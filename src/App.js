4405f53f8c5dafff34e9becc3f73e2bc5fc219d3
veronica2550 <128062548+veronica2550@users.noreply.github.com>
2024-10-02 20:24:59 +0900

    feat: 사용자 정의 태그 사용

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+//사용자 정의 태그는 function으로 만듦, 대문자로 시작해야함
+function Header() {
+  return <header>
+    <h1>
+      <a herf="/">WEB</a>
+    </h1>
+  </header>
+}
+function Nav() {
+  return <nav>
+    <ol>
+      <li>
+        <a href="/read/1">html</a>
+      </li>
+      <li>
+        <a href="/read/2">css</a>
+      </li>
+      <li>
+        <a href="/read/3">js</a>
+      </li>
+    </ol>
+  </nav>
+}
+function Article() {
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <h1>
-          <a herf="/">WEB</a>
-        </h1>
-      </header>
-      <nav>
-        <ol>
-          <li>
-            <a href="/read/1">html</a>
-          </li>
-          <li>
-            <a href="/read/2">css</a>
-          </li>
-          <li>
-            <a href="/read/3">js</a>
-          </li>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
